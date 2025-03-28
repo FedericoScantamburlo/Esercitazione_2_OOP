a2b15d56bfbffe8734c7ef54206b819ad634352c
FedericoScantamburlo <s310111@gmail.com>
2025-03-28 20:59:17 +0000

    Esercitazione svolta

---
+++ complex_number.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include "complex_number.hpp"
+
+int main() {
+	complex_number<float> z1 (1, 4);
+	cout << "z1 = " << z1 << endl;
+	cout << "Re(z1) = " << z1.re() << ", Im(z1) = " << z1.im() << endl;
+	complex_number<float> z2 (2);
+	cout << "z2 = " << z2 << endl;
+	complex_number<float> z3 (-5, 1);
+	cout << "z3 = " << z3 << endl;
+	cout << "Coniug(z3) = " << z3.coniug() << endl;
+	z1 += z2;
+	complex_number z5 = z1 + z3;
+	cout << "z4 = z1 + z2 = " << z1 << endl;
+	cout << "z5 = z3 + z4 = " << z5 << endl;
+	z1 *= z2;
+	complex_number z7 = z3 * z1;
+	cout << "z6 = z4 * z2 = " << z1 << endl;
+	cout << "z7 = z3 * z6 = " << z7 << endl;
+	complex_number<float> z8;
+	cout << "z8 = " << z8 << endl;
+	float r1 = 4;
+	cout << "r1 = " << r1 << endl;
+	z1 += r1;
+	complex_number z10 = z3 + r1;
+	cout << "z9 = z6 + r1 = " << z1 << endl;
+	cout << "z10 = z3 + z1 = " << z10 << endl;
+	float r2 = 3;
+	cout << "r2 = " << r2 << endl;
+	z10 *= r2;
+	complex_number z12 = z10 * r2;
+	cout << "z11 = z10 * r2 = " << z10 << endl;
+	cout << "z12 = z11 * r2 = " << z12 << endl;
+	return 0;
+}
---
+++ complex_number.hpp
@@ -0,0 +1,110 @@
+#include<iostream>
+using namespace std;
+
+template <typename T> requires std::floating_point<T>
+
+class complex_number
+{
+	T a, b;
+	
+public:
+	// default constructor
+	complex_number()
+		: a(0.0), b(0.0)
+	{}
+	
+	// user defined converting constructor
+	explicit complex_number(T x)
+		: a(x), b(0.0)
+	{
+		std::cout << "converting constructor called" << std::endl;
+    }
+	
+	// user defined constructor
+	complex_number(T x, T y)
+        : a(x), b(y)
+    {}
+	
+	// re, im, coniug methods
+	T re(void) const{
+		return a;
+	}
+	
+	T im(void) const{
+		return b;
+	}
+	
+	complex_number coniug(void) const{
+		return complex_number(a, -b);
+	}
+	
+	//operators += and +
+	complex_number& operator+=(const complex_number& z_2) {
+        T a_2 = z_2.re();
+        T b_2 = z_2.im();
+        a += a_2;
+        b += b_2;
+        return *this;
+	}
+	
+	complex_number operator+(const complex_number& z_2) {
+		complex_number z = *this;
+		z += z_2;
+		return z;
+	}
+	
+	complex_number& operator+=(const T& a_2) {
+		a += a_2;
+		return *this;
+	}
+	
+	complex_number operator+(const T& a_2) {
+		complex_number z = *this;
+		complex_number z_2 = complex_number(a_2);
+		z += z_2;
+		return z;
+	}
+		
+	
+	//operators *= and *
+	complex_number& operator*=(const complex_number& z_2) {
+		T a_2 = z_2.re();
+        T b_2 = z_2.im();
+		a = a*a_2 - b*b_2;
+		b = a*b_2 + b*a_2;
+		return *this;
+	}
+	
+	complex_number operator*(const complex_number& z_2) {
+		complex_number z = *this;
+		z *= z_2;
+		return z;
+	}
+	
+	complex_number& operator*=(const T& a_2) {
+		a *= a_2;
+		b *= a_2;
+		return *this;
+	}
+	
+	complex_number operator*(const T& a_2) {
+		complex_number z = *this;
+		complex_number z_2 = complex_number(a_2);
+		z *= z_2;
+		return z;
+	}	
+};
+
+//print
+template<typename T>
+std::ostream&
+operator<<(std::ostream& os, const complex_number<T>& z) {
+    if (abs(z.im()) < 1e-12)
+        os << z.re();
+    else if (abs(z.re()) < 1e-12)
+        os << z.im() << "i";
+	else 
+		os << z.re() << " + " << z.im() << "i"; 
+
+    return os;
+}
